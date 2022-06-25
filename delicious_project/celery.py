792e94d3e2e6df8243e943cf4a1dcc2cb3f45c42
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-06-25 23:44:41 +0300

    Added env variables

--- delicious_project/celery.py
+++ delicious_project/celery.py
@@ -1,17 +1,15 @@
 import os
-
+import dotenv
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'delicious_project.settings')
 
 app = Celery('delicious_project')
+app.config_from_object('django.conf:settings')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), '.env')
+dotenv.read_dotenv(env_file)
